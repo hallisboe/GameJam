85966cb13491dae771c08e7c32c5b2e5a7a5eb75
Anders Iversen <andis.boy@hotmail.com>
2017-11-04 00:46:41 +0100

    Changes sprites

--- js/sprites.js
+++ js/sprites.js
@@ -1,6 +1,6 @@
 class Sprites{
 
 	constructor(){
-		this.type0 = loadImage("sprites/type0.png");
+		this.type0 = loadImage("https://github.com/hallisboe/GameJam/blob/master/sprites/type0.png?raw=true");
 	}
 }
