9d5324409e65dbb0a40fce93a75c8f4aaf22d0dc
Anders Iversen <andis.boy@hotmail.com>
2017-11-03 20:58:03 +0100

    Did changes in player

--- js/main.js
+++ js/main.js
@@ -4,7 +4,6 @@
 
 let world;
 let player;
-let playerVel = {x: 0,y: 1};
 const SCALE = 50;
 
 let width = window.innerWidth
@@ -28,32 +27,33 @@ function draw(){
   background(51);
   world.draw();
 
-  player.update(playerVel);
+  player.update();
 }
 
 function keyPressed(){
   // Add player controls
+  console.log("KeyCode: " + keyCode);
   if(keyCode === 87){
-  	playerVel.y = -1;
+  	player.vel.y = -1;
   }
   else if(keyCode === 83){
-  	playerVel.y = 1;
+  	player.vel.y = 1;
   }
   else if(keyCode === 65){
-  	playerVel.x = 1;
+  	player.vel.x = -1;
   }
-  else if(keyCode === 67){
-  	playerVel.x = -1;
+  else if(keyCode === 68){
+  	player.vel.x = 1;
   }
 }
 
 function keyReleased(){
   // Stop player
   if(keyCode === 87 || keyCode === 83){
-  	playerVel.y = 0;
+  	player.vel.y = 0;
   }
-  else if(keyCode === 65 || keyCode === 67){
-  	playerVel.x = 0;
+  else if(keyCode === 65 || keyCode === 68){
+  	player.vel.x = 0;
   }
 }
 
