1652335c7fdb52a5ccdd5f7c23d607a86193df60
halvorboe <halvor98@outlook.com>
2017-11-04 17:53:17 +0100

    Test

--- js/enemy.js
+++ js/enemy.js
@@ -11,33 +11,27 @@ class Enemy {
 	}
 
 	draw(){
+		//console.log(this.pos)
 		fill(0);
 		ellipse(this.pos.x - this.r, this.pos.y - this.r,this.r*2,this.r*2);
 	}
 
 	moveTowards(target){
-		let targetPos = target.pos;
-		if(abs(targetPos.x - this.pos.x) >= this.minDistance || abs(targetPos.y - this.pos.y) >= this.minDistance){
-			let x = targetPos.x - this.pos.x;
-			let y = targetPos.y - this.pos.y;
-			let magnitude = sqrt(pow(x,2) + pow(y,2));
-			this.vel.x = (x/magnitude)*this.speed;
-			this.vel.y = (y/magnitude)*this.speed;
+		if(abs(target.pos.x - this.pos.x) >= this.minDistance || abs(target.pos.y - this.pos.y) >= this.minDistance){
 
-			this.pos.y += this.vel.y;
-			this.pos.x += this.vel.x;
 		}
-		else if(target.reduceHealth() && this.attackElapsed >= this.attackRate){
+		else if(target.building.reduceHealth() && this.attackElapsed >= this.attackRate){
 			this.attackElapsed = 0;
-			target.reduceHealth();
+			target.building.reduceHealth();
 		}
 		else{
 			this.attackElapsed++;
 		}
 	}
 
-	update(target){
+	update(target, pos){
 		this.moveTowards(target);
+		this.pos = pos;
 		this.draw();
 	}
 }
