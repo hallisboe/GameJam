3b46e9fbef571f70da7a29bcf78fb7dd5f8d70ac
unknown <seband95@gmail.com>
2017-11-04 19:10:04 +0100

    Removed debug

--- js/world.js
+++ js/world.js
@@ -56,11 +56,11 @@ class World {
 
 	draw(){
 		image(sprite.world, 0, 0);
-        for(let x = 0; x < this.tiles.length; x++) {
-            for(let y = 0; y < this.tiles[0].length; y++) {
-                if(this.tiles[x][y] == 2) rect(x * SCALE, y * SCALE, SCALE, SCALE);
-            }
-        }
+        // for(let x = 0; x < this.tiles.length; x++) {
+        //     for(let y = 0; y < this.tiles[0].length; y++) {
+        //         if(this.tiles[x][y] == 2) rect(x * SCALE, y * SCALE, SCALE, SCALE);
+        //     }
+        // }
 	}
 
 }
