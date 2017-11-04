42ba382d77ece3be7a86af49444fb045156ecf6c
Anders Iversen <andis.boy@hotmail.com>
2017-11-04 01:47:05 +0100

    UI

--- js/gui.js
+++ js/gui.js
@@ -1,16 +1,20 @@
 class GUI{
 
-	constructor(){
-
+	constructor(inventory){
+		this.inventory = inventory;
 	}
 
 	drawTopBar(){
-		fill(100);
-		rect(0,0,width,30);
+		fill(255,153,51);
+		rect(0,0,width,40);
+		fill(255);
+		rect(25,15,10,10);
+		fill(0);
+		text(inventory.r1,50,25);
 	}
 
 	draw(){
-		
+		this.drawTopBar();
 	}
 
 }
--- js/main.js
+++ js/main.js
@@ -6,6 +6,8 @@ let world;
 let player;
 let enemyController;
 let sprites;
+let inventory;
+let gui;
 const SCALE = 50;
 const enemyLimit = 10;
 
@@ -34,6 +36,8 @@ function setup() {
   player = new Player({x: width/2, y: height/2});
   enemyController = new EnemyController(enemyLimit);
   building = new Building({x: 200, y: 200});
+  inventory = new Inventory();
+  gui = new GUI(inventory);
 }
 
 function draw(){
@@ -43,6 +47,8 @@ function draw(){
   player.checkCollision(enemyController.enemies);
   enemyController.update(building);
   building.draw();
+
+  gui.draw(); //Have to be the last one to draw
 }
 
 function keyPressed(){
