773aee9db676dd5536840fe54906ebf28ca996ab
unknown <seband95@gmail.com>
2017-11-05 03:49:03 +0100

    updated sprites

--- js/sprites.js
+++ js/sprites.js
@@ -7,12 +7,13 @@ class Sprites{
 		this.playerFront = loadImage("http://i.imgur.com/iHTCY7t.png");
 		this.playerBack = loadImage("http://i.imgur.com/BGe64zl.png");
 		this.world = loadImage("https://i.imgur.com/vb38qP3.png");
-		this.world2 = loadImage("https://i.imgur.com/OslrKlb.png"); 
+		this.world2 = loadImage("https://i.imgur.com/OslrKlb.png");
 		this.portal = loadImage("http://i.imgur.com/3nyHBOM.png");
 		this.miner = loadImage("https://i.imgur.com/1AwAPLS.png");
 		this.miner2 = loadImage("https://i.imgur.com/MwKbZn4.png");
 		this.turretStand = loadImage("https://i.imgur.com/QpR1BCA.png");
 		this.turretRed = loadImage("https://i.imgur.com/DaKTwhC.png");
 		this.turretGreen = loadImage("https://i.imgur.com/xXFRJhM.png");
+		this.enemy = loadImage("https://i.imgur.com/6uPwrDl.png");
 	}
 }
