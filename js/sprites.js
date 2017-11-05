b4214069243e9ea01e3b1fdab061b825320144fb
halvorboe <halvor98@outlook.com>
2017-11-05 09:48:27 +0100

    test

--- js/sprites.js
+++ js/sprites.js
@@ -1,22 +1,20 @@
 class Sprites{
 
 	constructor(){
-		this.dirt = loadImage("http://i.imgur.com/x2OngvR.png");
 		this.playerLeft = loadImage("http://i.imgur.com/4Z6xoYK.png");
 		this.playerRight = loadImage("http://i.imgur.com/Z8U1FJ2.png");
 		this.playerFront = loadImage("http://i.imgur.com/iHTCY7t.png");
 		this.playerBack = loadImage("http://i.imgur.com/BGe64zl.png");
-		this.world = loadImage("https://i.imgur.com/vb38qP3.png");
-		this.world2 = loadImage("https://i.imgur.com/OslrKlb.png");
+		this.world = loadImage("https://i.imgur.com/4EDY5Ix.png");
+		this.world2 = loadImage("https://i.imgur.com/n04iZjv.png");
 		this.portal = loadImage("https://i.imgur.com/l7ftCpx.png");
-		this.miner = loadImage("https://i.imgur.com/1AwAPLS.png");
-		this.miner2 = loadImage("https://i.imgur.com/MwKbZn4.png");
-		this.turretStand = loadImage("https://i.imgur.com/QpR1BCA.png");
-		this.turretRed = loadImage("https://i.imgur.com/DaKTwhC.png");
-		this.turretGreen = loadImage("https://i.imgur.com/xXFRJhM.png");
-		this.enemy = loadImage("https://i.imgur.com/Gm9EVxD.png");
-		this.r1 = loadImage("https://i.imgur.com/jg7QJE7.png");
-		this.r2 = loadImage("https://i.imgur.com/qFlvXAo.png");
-		this.enemy = loadImage("https://i.imgur.com/Vdbex7Z.png");
+		this.miner = loadImage("https://i.imgur.com/WxmAAO6.png");
+		this.miner2 = loadImage("https://i.imgur.com/jRUnBGm.png");
+		this.turretStand = loadImage("https://i.imgur.com/Ywi4XTU.png");
+		this.turretRed = loadImage("https://i.imgur.com/TW4OIlI.png");
+		this.turretGreen = loadImage("https://i.imgur.com/FAwdFpy.png");
+		this.enemy = loadImage("https://i.imgur.com/EarOBUE.png");
+		this.r1 = loadImage("https://i.imgur.com/p6u3LnA.png");
+		this.r2 = loadImage("https://i.imgur.com/YoYeuvj.png");
 	}
 }
